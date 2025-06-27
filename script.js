6aa204b0757b0ac965d3455ff25a18fe61ff67ab
romanmorales-03 <daniel.morales.torres@u-erre.mx>
2025-06-26 18:57:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+function validarFormulario() {
+    const clave = parseInt(document.getElementById("lclave").value.trim());
+    const nombre = document.getElementById("fnom").value.trim();
+    const appat = document.getElementById("lappat").value.trim();
+    const apmat = document.getElementById("lapmat").value.trim();
+
+    if (isNaN(clave) || clave <= 0) {
+        alert("La clave debe ser un número mayor a 0.");
+        return false;
+    }
+
+    if (nombre === "") {
+        alert("El campo Nombre no puede estar vacío.");
+        return false;
+    }
+    if (appat === "") {
+        alert("El campo Apellido Paterno no puede estar vacío.");
+        return false;
+    }
+    if (apmat === "") {
+        alert("El campo Apellido Materno no puede estar vacío.");
+        return false;
+    }
+
+    // todo correcto
+    return true;
+}
+
+
+
 function convertirNomMayusculas(){
     				  const inputElement = document.getElementById("fnom");
   					  inputElement.value = inputElement.value.toUpperCase();
