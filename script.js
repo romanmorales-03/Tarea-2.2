1a5e00b63f197654a7b5599189e152cd7d40c819
romanmorales-03 <daniel.morales.torres@u-erre.mx>
2025-06-26 18:33:24 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function convertirNomMayusculas(){
+    				  const inputElement = document.getElementById("fnom");
+  					  inputElement.value = inputElement.value.toUpperCase();
+    			}
+
+function convertirApePatMayusculas(){
+    				  const inputElement = document.getElementById("lappat");
+  					  inputElement.value = inputElement.value.toUpperCase();
+    			}
+
+function convertirApeMatAMayusculas(){
+    				  const inputElement = document.getElementById("lapmat");
+  					  inputElement.value = inputElement.value.toUpperCase();
+    			}
